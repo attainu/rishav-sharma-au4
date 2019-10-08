0f6010515d1e91a96f03313050aba536d70c659a
rishav-sharma-vst-au4 <54503782+rishav-sharma-vst-au4@users.noreply.github.com>
2019-10-08 10:39:42 +0530

    weekly test

--- test/weekly/week4/login.js
+++ test/weekly/week4/login.js
@@ -1,25 +1,25 @@
 var globalArray=[];
 $(document).ready(function(){
-    $('#form-Submit').on('click',function(){
+    $('#Submit').on('click',function(){
         var nameValue = $('#UserName').val();
         var ageValue = $('#Age').val();
-        globalArray.push(nameValue);
+       /* globalArray.push(nameValue);
         for(var i=0;i<globalArray.length;i++){
             var a=globalArray[i];
         if(nameValue==a){
-            $('#value').html('Name already exist!');
+            $('#value').text('Name already exist!');
         } 
            }
         if(!nameValue){
-            $('#value').html('Please input name');
+            $('#value').text('Please input name');
             return false;
         }
        if(ageValue<15){
-            $('#value').html('Age should be more than 15');
+            $('#value').text('Age should be more than 15');
             return false;
 
-        }else{
-            $('<li/>').html(nameValue + ' '+ageValue).appendTo('#value');
+        }else{*/
+            $('<li/>').text(nameValue + ' '+ageValue).appendTo('#value');
             return true;
         } 
     });
