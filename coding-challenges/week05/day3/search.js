6111878ef5e086049b06715831b08674a3535635
rishav-sharma-vst-au4 <54503782+rishav-sharma-vst-au4@users.noreply.github.com>
2019-10-11 18:05:32 +0530

    cc

--- coding-challenges/week05/day3/search.js
+++ coding-challenges/week05/day3/search.js
@@ -1,13 +1,31 @@
-$(document).ready(function() {
-     // Write your app logic below this line
-     
- 
-     //assignment start
-     $.ajax({
-         url:'./books.json',
-         success:function(data) {
-             console.log(data)
- 
-            var arr = Object.keys(data);
-             for (var i=0;i < arr.length; i++) {
-              
+var books = [];
+
+   $.ajax({
+
+   url : "./books.json",
+   method : "GET",
+   datatype : "json",
+   success: function(data) {
+       console.log(data);
+      data = JSON.parse(data);
+      for(var i = 0; i < data.length; i++){
+          books.push(data[i]);
+          }
+   }
+   });
+   console.log(books);
+
+   $("process-submit").on('click',function(){    
+
+           $("tbody").html("");
+
+           for(var i = 0; i < books.length; i++){
+
+               if(books[i].language.toLowerCase() == $("#search-keyword").val().toLowerCase()){
+                   $("tbody").append('<tr><td>' + book[i].title + '</td><td>' + book[i].author + '</td><td>' + book[i].country + 
+                   '</td><td>' + book[i].language + '</td><td>' + book[i].link + '</td><td>' + book[i].page+ '</td><td>' +
+                    book[i].page + '</td><td>');
+
+               }
+       }
+   });
