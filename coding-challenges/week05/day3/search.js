72502d3a60a13069cebf8a37a187d43b8b5e3207
rishav-sharma-vst-au4 <54503782+rishav-sharma-vst-au4@users.noreply.github.com>
2019-10-13 01:22:13 +0530

    cc

--- coding-challenges/week05/day3/search.js
+++ coding-challenges/week05/day3/search.js
@@ -7,15 +7,15 @@ var books = [];
    datatype : "json",
    success: function(data) {
        //console.log(data);
-      data = JSON.parse(data);
+      //data = JSON.parse(data);
       for(var i = 0; i < data.length; i++){
           books.push(data[i]);
           }
-   }
+        }
    });
    console.log(books);
 
-   $("process-submit").on('click',function(){    
+   $("#submit-btn").on('click', function(){    
 
            $("tbody").html("");
 
@@ -23,9 +23,9 @@ var books = [];
 
                if(books[i].language.toLowerCase() == $("#search-keyword").val().toLowerCase()){
                    $("tbody").append('<tr><td>' + book[i].title + '</td><td>' + book[i].author + '</td><td>' + book[i].country + 
-                   '</td><td>' + book[i].language + '</td><td>' + book[i].link + '</td><td>' + book[i].page+ '</td><td>' +
-                    book[i].page + '</td><td>');
+                   '</td><td>' + book[i].language + '</td><td>' + book[i].link + '</td><td>' + book[i].page + '</td><td>' +
+                    book[i].year + '</td><td>');
 
-               }
+            }
        }
    });
