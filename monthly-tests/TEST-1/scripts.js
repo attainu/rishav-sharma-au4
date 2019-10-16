ed6bb22b4114fd4b9f252d18f4d501a451a15016
rishav-sharma-vst-au4 <54503782+rishav-sharma-vst-au4@users.noreply.github.com>
2019-10-16 18:28:29 +0530

    Update scripts.js

--- monthly-tests/TEST-1/scripts.js
+++ monthly-tests/TEST-1/scripts.js
@@ -1,51 +1,25 @@
-/*$(document).ready(function(){
+//employee list
+   $(document).ready(function () {
+    
+    var employee_data = [];
+    $.ajax({
+      url : "./employee_data.json",
+      method : "GET",
+      datatype : "employee_data",
+      success: function(employee_data) {
 
-  $.getJSON("employee_data.json",fuction(data){
-    var employee_data ='';
-    $.each(data,function(key, value){
-      employee_data += '<tr>';
-      employee_data += '<td>' + value.name+ '</td>';
-      employee_data += '<td>' + value.E-MAIL+ '</td>';
-      employee_data += '<td>' + value.name+ '</td>';
-      employee_data += '<td>' + value.name+ '</td>';
-    })
-
-
+   
+    var tr;
+    for (var i = 0; i < employee_data.length; i++) {
+      tr = $('<tr/>');
+      tr.append("<td>" +  [i+1]+ "</td>");
+      tr.append("<td>" + employee_data[i].name + "</td>");
+      tr.append("<td>" + employee_data[i].email + "</td>");
+      tr.append("<td>" + employee_data[i].country + "</td>");
+      tr.append("<td>" + employee_data[i].salary + "</td>");
+      tr.append("<td>" + "<button class='btn btn-danger' >Delete</button>" + "</td>");
+      $('tbody').first().append(tr);
+    }
+  }  
   });
-});*/
-
-
-
-
-
-
-
-var employee_data = [];
-
-   $.ajax({
-     url : "./employee_data.json",
-     method : "GET",
-     datatype : "JSON",
-     success: function(employee_data) {
-       //console.log(data);
-      //data = JSON.parse(data);
-      for(var i = 0; i < employee_data.length; i++) {
-        data.push(employee_data[i]);
-          }
-        }
-   });
-   console.log(employee_data);
-
-   //$("#submit-btn").on('click', function() {    
-
-           $("tbody").html("");
-
-           for(var i = 0; i < employee_data.length; i++) {
-
-               
-                   $("tbody").append('<tr><td>' + employee_data[i].ID + '</td><td>' + employee_data[i].NAME + '</td><td>' + employee_data[i].E-MAIL + 
-                   '</td><td>' + employee_data[i].COUNTRY + '</td><td>' + employee_data[i].SALARY+ '</td><td>' + employee_data[i].ACTION + '</td><td>');
-
-            }
-       //}
-   //});*/
+});
