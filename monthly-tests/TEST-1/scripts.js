350d1310a5fe734a08ae22144727d005c54fe089
rishav-sharma-vst-au4 <54503782+rishav-sharma-vst-au4@users.noreply.github.com>
2019-10-16 18:08:05 +0530

    t

--- monthly-tests/TEST-1/scripts.js
+++ monthly-tests/TEST-1/scripts.js
@@ -1,30 +1,51 @@
-var data = [];
+/*$(document).ready(function(){
+
+  $.getJSON("employee_data.json",fuction(data){
+    var employee_data ='';
+    $.each(data,function(key, value){
+      employee_data += '<tr>';
+      employee_data += '<td>' + value.name+ '</td>';
+      employee_data += '<td>' + value.E-MAIL+ '</td>';
+      employee_data += '<td>' + value.name+ '</td>';
+      employee_data += '<td>' + value.name+ '</td>';
+    })
+
+
+  });
+});*/
+
+
+
+
+
+
+
+var employee_data = [];
 
    $.ajax({
-     url : "./data.json",
+     url : "./employee_data.json",
      method : "GET",
      datatype : "JSON",
-     success: function(data) {
-       console.log(data);
+     success: function(employee_data) {
+       //console.log(data);
       //data = JSON.parse(data);
-      for(var i = 0; i < data.length; i++) {
-          data.push(data[i]);
+      for(var i = 0; i < employee_data.length; i++) {
+        data.push(employee_data[i]);
           }
         }
    });
-   console.log(data);
+   console.log(employee_data);
 
-   /*$("#submit-btn").on('click', function() {    
+   //$("#submit-btn").on('click', function() {    
 
            $("tbody").html("");
 
-           for(var i = 0; i < books.length; i++) {
+           for(var i = 0; i < employee_data.length; i++) {
 
-               if(books[i].language.toLowerCase() == $("#search-keyword").val().toLowerCase()){
-                   $("tbody").append('<tr><td>' + books[i].title + '</td><td>' + books[i].author + '</td><td>' + books[i].country + 
-                   '</td><td>' + books[i].language + '</td><td>' + books[i].link + '</td><td>' + books[i].pages + '</td><td>' +
-                    books[i].year + '</td><td>');
+               
+                   $("tbody").append('<tr><td>' + employee_data[i].ID + '</td><td>' + employee_data[i].NAME + '</td><td>' + employee_data[i].E-MAIL + 
+                   '</td><td>' + employee_data[i].COUNTRY + '</td><td>' + employee_data[i].SALARY+ '</td><td>' + employee_data[i].ACTION + '</td><td>');
 
             }
-       }
-   });*/
+       //}
+   //});*/
