3b66fe060b04109e14f999c020fb4152b92f8c7e
rishav-sharma-vst-au4 <54503782+rishav-sharma-vst-au4@users.noreply.github.com>
2019-10-24 18:28:56 +0530

    cc

---
+++ coding-challenges/week07/day 3/index.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const chalk = require('chalk');
+const express = require('express'); // function
+const app = express();
+const PORT = 9090;
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use('/static', express.static('public'));
+
+app.get('/', function(req, res) {
+	res.status(200).send("Hello World! This is the homepage.");
+});
+
+app.get('/hello', function(req, res) {
+	console.log("Query parameters >>>", req.query);
+
+	var name = req.query.name;
+
+	res.send(name);
+});
+
+app.post('/login', function(req, res) {
+	console.log("POST DATA >>>>", req.body);
+});
+
+app.get('/login', function(req, res) {
+	res.sendFile(__dirname + '/login.html');
+});
+
+app.listen(PORT, function() {
+	console.log("Application has started and running on port: ", PORT);
+}).on('error', function(error) {
+	console.log("Unable to start app. Error >>>>", error);
+});
