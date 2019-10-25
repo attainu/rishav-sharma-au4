fb4a85dcf869e134ffdd84f983cbfa9a4f770f56
rishav-sharma-vst-au4 <54503782+rishav-sharma-vst-au4@users.noreply.github.com>
2019-10-25 20:17:29 +0530

    cc

--- coding-challenges/week07/day 3/index.js
+++ coding-challenges/week07/day 3/index.js
@@ -7,6 +7,20 @@ var port = 9090;
 //body-parser middleware
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(bodyParser.json());
+
+//register middleware
+app.use('/register',function(req,res,next){
+    if(Object.keys(req.body).length === 0) {
+        res.status(404);
+        res.send("No name and password  --> bad request");
+    }
+    if(req.body.password !== req.body.confirmpassword){
+        res.status(400).send("password not matching, Error status 400 -->Bad resquest");
+    }
+    next();
+});
+
+
 //home route
 app.get('/',function(req,res){
     res.send("Hello Welcome To Login Page");
