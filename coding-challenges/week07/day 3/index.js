16e67dcb0506c05a93cb6351749f360471b831e7
rishav-sharma-vst-au4 <54503782+rishav-sharma-vst-au4@users.noreply.github.com>
2019-10-25 19:55:04 +0530

    cc

--- coding-challenges/week07/day 3/index.js
+++ coding-challenges/week07/day 3/index.js
@@ -1,42 +1,23 @@
-const fs = require('fs');
-const chalk = require('chalk');
-const express = require('express'); // function
-const app = express();
-const PORT = 9090;
+var express = require('express');
+var bodyParser = require('bodyParser');
 
-app.use(express.json());
-app.use(express.urlencoded());
-app.use('/static', express.static('public'));
+var app = express();
+var port = 9090;
 
-app.get('/', function(req, res) {
-	res.status(200).send("Hello World! This is the homepage.");
+//body-parser middleware
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+//home route
+app.get('/',function(req,res){
+    res.send("Hello Welcome To Login Page");
 });
 
-app.get('/hello', function(req, res) {
-	console.log("Query parameters >>>", req.query);
-
-	var name = req.query.name;
-
-	res.send(name);
+// register route
+app.post('/register',function(req,res){
+    console.log(req.body);
+    res.send(req.body);
 });
 
-app.post('/login', function(req, res) {
-	console.log("POST DATA >>>>", req.body);
-});
-
-app.get('/login', function(req, res) {
-	res.sendFile(__dirname + '/login.html');
-});
-app.post('/register', function(req, res) {
-	console.log("POST DATA >>>>", req.body);
-});
-
-app.get('/register', function(req, res) {
-	res.sendFile(__dirname + '/register.html');
-});
-
-app.listen(PORT, function() {
-	console.log("Application has started and running on port: ", PORT);
-}).on('error', function(error) {
-	console.log("Unable to start app. Error >>>>", error);
+app.listen(post,function(req,res){
+    console.log('server started at port ${post}');
 });
