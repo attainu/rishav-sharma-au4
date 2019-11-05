6bc73b993824cb6405d9f993cb54380ce66b1216
rishav-sharma-vst-au4 <54503782+rishav-sharma-vst-au4@users.noreply.github.com>
2019-11-05 18:00:21 +0530

    ass

---
+++ assignments/week09/day1/models/Auth.js
@@ -0,0 +1,8 @@
+const Auth = {};
+
+Auth.create = function (username, password) {
+	console.log(`${password} created a book : ${username}`)
+
+}
+
+module.export = Auth;
---
+++ assignments/week09/day1/routes/auth.js
@@ -0,0 +1,43 @@
+const AuthController = {};
+var model = require('../models/Auth')
+
+AuthController.login = function(request, response){
+    request.session.loggedIn = true;
+        response.json({
+            "status": true,
+            "message": "loggedIn in the system"
+        });
+    }
+    AuthController.login = function(request, response){
+    var username = request.body.username;
+    var password = request.body.password;
+    if(req.session.loggedIn) {
+        model.create(username,password);
+        response.json({
+         status: true,
+        message: "successfully you logged in to create book"
+    });
+} else {
+    response.json({
+        "status": false,
+        "message": "please login to create book"
+    });
+}
+    }
+AuthController.logout = function(request, response) {
+    if (request.session.loggedin){
+        request.session.destroy(function(err){
+            if(err) {
+                next(err)
+            } else {
+                response.clearCookie('reconnecting.com')
+                response.json({
+                    "status": true,
+                    "message": "Loggedout  very smartly"
+                });
+            }
+        });
+    }
+
+}
+module.exports = AuthController;
