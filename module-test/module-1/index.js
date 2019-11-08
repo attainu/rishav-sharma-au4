8f4d4b68c85d491a65102b34215ec7cbe85fa9be
rishav-sharma-vst-au4 <54503782+rishav-sharma-vst-au4@users.noreply.github.com>
2019-11-08 15:12:55 +0530

    test

---
+++ module-test/module-1/index.js
@@ -0,0 +1,62 @@
+const express = require('express'); // function
+const app = express();
+const exphbs = require('express-handlebars');
+const PORT = 9090;
+const MongoClient = require('mongodb').MongoClient;
+const csvtojson = require("csvtojson");
+const Filesystem = require("fs");
+
+csvtojson().fromFile("./data.csv").then(users => {
+    console.log(data);
+
+const json = csvtojson(source, {fields: ["username","name","age","city"] });
+Filesystem.writeFileSync("./users.json", json);
+});
+
+ 
+app.use(express.json());
+app.use('/static',express.static('public'));
+
+// Configure Handlebars
+const hbs = exphbs.create({
+	extname: '.hbs'
+});
+
+
+// Register Handlebars as view engine
+app.engine('.hbs', hbs.engine);
+app.set('view engine', '.hbs');
+
+
+// Connection URL
+var url = 'mongodb://localhost:27017';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+    if(error) {
+        throw error;
+    }
+    var db = client.db('eagle-rishav-sharma');
+    var collection = db.collection('users');
+    collection.find({}, function(error, response){
+        console.error(error);
+        console.log(response);
+
+    });
+});
+
+app.get('/username', function(req,res){
+    var data = books;
+    
+    res.render('search',{
+        data: result
+    });
+    });
+    
+    
+    // Start the app on pre defined port number
+    app.listen(PORT, function() {
+        console.log("Application has started and running on port: ",chalk.blue(PORT));
+    }).on('error', function(error) {
+        console.log("Unable to start app. Error >>>>", error);
+    });
