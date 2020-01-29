dc46bcc1277d081bdfd6389baf6fe46e8319ec28
rishav-sharma-vst-au4 <54503782+rishav-sharma-vst-au4@users.noreply.github.com>
2020-01-29 20:01:21 +0530

    test

---
+++ monthly-tests/january-2020/day 3/index.js
@@ -0,0 +1,31 @@
+// O(n) time & O(n) space
+function mergeTwo(arr1, arr2) {
+    let merged = [];
+    let index1 = 0;
+    let index2 = 0;
+    let current = 0;
+  
+    while (current < (arr1.length + arr2.length)) {
+  
+      let isArr1Depleted = index1 >= arr1.length;
+      let isArr2Depleted = index2 >= arr2.length;
+  
+      if (!isArr1Depleted && (isArr2Depleted || (arr1[index1] < arr2[index2]))) {
+        merged[current] = arr1[index1];
+        index1++;
+      } else {
+        merged[current] = arr2[index2];
+        index2++;
+      }
+  
+      current++;
+    }
+  
+    return merged;
+  }
+  arr1=[1,2,4];
+  arr2=[1,3,4];
+ console.log(mergeTwo(arr1,arr2));
+
+
+ 
