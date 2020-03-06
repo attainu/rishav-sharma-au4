13bc0547a7885b799705baddb6b221098a718f84
rishav-sharma-vst-au4 <54503782+rishav-sharma-vst-au4@users.noreply.github.com>
2020-03-06 17:54:19 +0530

    h

--- assignments/week22/day 3/userdetails/src/App.js
+++ assignments/week22/day 3/userdetails/src/App.js
@@ -1,26 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  state = {
+    name: "",
+    email: "",
+    namelist: [],
+    emaillist:[],
+    showValue: "all"
+  };
+
+  SetSpan = event => {
+    this.setState({
+      name: event.target.value
+    });
+  };
+
+  Setemail= event => {
+    this.setState({
+      email: event.target.value
+    });
+  };
+
+  updateList = () => {
+    let list = this.state.namelist
+    list.push(this.state.name);
+    let list1=this.state.emaillist
+    list1.push(this.state.email)
+    this.setState({
+      name: "",
+      email: "",
+      namelist: list,
+      emaillist:list1
+    });
+    console.log(this.state.namelist)
+    console.log(this.state.emaillist)
+  };
+
+  SetShowList = event => {
+    this.setState({
+      showValue: event.target.value
+    });
+  };
+
+  render() {
+    var {namelist,emaillist}=this.state
+    return (
+      <div>
+        <input
+          type="string"
+          onChange={event => this.SetSpan(event)}
+          value={this.state.name}
+        />
+        <input
+          type="string"
+          onChange={event => this.Setemail(event)}
+          value={this.state.email}
+        />
+        {/*  */}
+        
+        <select onChange={event => this.SetShowList(event)} id="day">
+          <option value="all">All</option>
+          <option value="0">Mumbai</option>
+          <option value="1">Kolkata</option>
+          <option value="2">Delhi</option>
+          <option value="3">Bangalore</option>
+          <option value="4">Mangalore</option>
+          <option value="5">Goa</option>
+          <option value="6">Patna</option>
+        </select>
+        <button
+          onClick={() => this.updateList()}
+          type="button"
+          disabled={
+            this.state.name.length === 0 || this.state.email.length === 0
+          }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Submit
+        </button>
+        <span>Element:Name:-{this.state.name},Email:-{this.state.email}</span>
+        <span>
+          Name:-
+          {
+            namelist.map(el=><ul>{el}</ul>)
+          }
+        </span>
+        <span>Email:-
+          {
+            emaillist.map(el=><ul>{el}</ul>)
+          }
+        </span>
+      </div>
+    );
+  }
 }
 
 export default App;
