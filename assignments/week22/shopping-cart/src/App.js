e78d193f0229ea108f616325505062793e98760f
rishav-sharma-vst-au4 <54503782+rishav-sharma-vst-au4@users.noreply.github.com>
2020-03-23 00:19:32 +0530

    assign

--- assignments/week22/shopping-cart/src/App.js
+++ assignments/week22/shopping-cart/src/App.js
@@ -1,57 +1,24 @@
-import React,{Component,Fragment}from 'react';
-import StaticData from './components/StaticData';
-import Cart from './components/Cart';
+import React, { Component } from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Cart from './components/Cart'
 
 class App extends Component {
-  state = {
-    items: [
-      { id: "1", name: "Orange", cost: 5},
-      { id: "2", name: "Banana", cost: 18},
-      { id: "3", name: "Chiku", cost: 6},
-      { id: "4", name: "Pomegranate", cost: 2},
-      { id: "5", name: "Pears", cost: 12},
-      { id: "6", name: "Apple", cost: 50},
-      { id: "7", name: "Mausami", cost: 125},
-      { id: "8", name: "PineApple", cost: 99},
-    ],
-    cart:[]
-  }
-
-  addToCart = (e) => {
-    const id = e.target.value;
-    const newItem = this.state.items.filter(item => item.id === id);
-     this.setState({
-      cart: [...this.state.cart, newItem[0]]
-    })
-  }
-  deleteFromCart = (e) => {
-    const id = e.target.value;
-    const restofItems = this.state.cart.filter(
-      (item, idx) => idx.toString !== id
-    );
-    this.setState({
-      cart: restofItems
-    })
-
-  }
   render() {
     return (
-      <Fragment>
-        <div className="container-fluid bg-dark text-white text-center">
-          <h1 className="display-4">Cart Assignment</h1>
-        </div>
-        <div className="container">
-          <div className="row">
-            <div className="col-6">
-              <StaticData items={this.state.items} add={this.addToCart} />
-            </div>
-            <div className="col-6">
-              <Cart carts={this.state.cart} delete={this.deleteFromCart}/>
-            </div>
-          </div>
-        </div>
-      </Fragment>
-    )
+       <BrowserRouter>
+            <div className="App">
+            
+              <Navbar/>
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/cart" component={Cart}/>
+                  </Switch>
+             </div>
+       </BrowserRouter>
+      
+    );
   }
 }
 
--- assignments/week22/shopping-cart/src/index.js
+++ assignments/week22/shopping-cart/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css'
+import cartReducer from './components/reducers/cartReducer';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(cartReducer);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
