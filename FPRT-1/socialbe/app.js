61edda3ba28e059952bc65404e553e079c600f41
rishav-sharma-vst-au4 <54503782+rishav-sharma-vst-au4@users.noreply.github.com>
2020-06-08 05:24:18 +0530

    fprt-1 only backend

---
+++ FPRT-1/socialbe/app.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const express = require('express')
+const app = express()
+const mongoose  = require('mongoose')
+const PORT = process.env.PORT || 5000
+
+
+mongoose.connect(process.env.MONGOURI,{
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+
+})
+mongoose.connection.on('connected',()=>{
+    console.log("conneted to mongodb jio :)")
+})
+mongoose.connection.on('error',(err)=>{
+    console.log("err connecting",err)
+})
+
+require('./models/user')
+require('./models/post')
+
+app.use(express.json())
+app.use(require('./routes/auth'))
+app.use(require('./routes/post'))
+app.use(require('./routes/user'))
+
+
+
+
+app.listen(PORT,()=>{
+    console.log("server is running on port",PORT)
+})
+
---
+++ FPRT-1/socialbe/config/keys.js
@@ -0,0 +1,5 @@
+// MONGODBURL=mongodb+srv://fprtsocial:12345@fprtsocial-sleqh.gcp.mongodb.net/fprt?retryWrites=true&w=majority
+// API_KEY=412777663376211
+// API_SECRET=mJSqIuElnyRACcKXWDoiM6nWhO
+// JWT_TOKEN=sdgf1234456adsff
+// JWT_SECRET=sdvas32red
